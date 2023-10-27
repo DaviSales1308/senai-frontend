27c937527927aed58cb443f1083eb5668792cb45
DaviSales <davi.armando@hotmail.com>
2023-10-27 20:25:50 -0300

    começando o js

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+import {} from "./../../data/data.js"
+
+let portifolioName = document.querySelector(".js-portifolio-name");
+let portifolioRole = document.querySelector(".js-portifolio-role");
+let resumeText = document.querySelector(".js-resume-text");
+
+portifolioName.innerText =  portifolioName
+portifolioRole.innerText = portifolioRole
+resumeText.innerText = resumeText
+console.log(portifolio.username)
---
+++ data/data.js
@@ -0,0 +1,18 @@
+const portifolio ={
+    username: "Davi Sales",
+    role: "Estudante",
+    social: [],
+    resume: "Lorem ipsum dolor sit amet consectetur adipisicing elit."+
+    "Quam aspernatur iusto repellendus magni" +
+    "beatae enim maxime est fuga ad, nostrum reprehenderit," +
+    "autem nisi quaerat quisquam debitis blanditiis, mollitia eaque aliquam.",
+    projects:[
+        {name:"", tools:[], url:""},
+        {name:"", tools:[], url:""},
+        {name:"", tools:[], url:""},
+        {name:"", tools:[], url:""},
+        {name:"", tools:[], url:""}
+    ]
+}
+
+export{portifolio}
